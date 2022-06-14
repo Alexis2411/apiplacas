ca6fcb2155b7827b454abdf398bd77d80dadf6d7
Alexis2411 <zs19004866@estudiantes.uv.mx>
2022-06-14 16:54:33 -0500

    act

--- app/configuraciondb/db.config.js
+++ app/configuraciondb/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "ec2-52-72-99-110.compute-1.amazonaws.com",
-    USER: "qsolkgmiyzgomg",
-    PASSWORD: "a245b33630faf32112ce2f909643aaca30d8812fbe81ec1dd33ffa5eb295499f",
-    DB: "d89dugp9vt63lq",
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "1234",
+    DB: "api",
     dialect: "postgres",
     pool: {
         Max: 5,
